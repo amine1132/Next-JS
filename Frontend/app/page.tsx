b071779aa1d7a2f130d198e7fc4b92ecdcaa6875
Amine Sekkiou <amine1132@hotmail.fr>
2024-07-30 20:34:31 +0200

    Routing

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import Bitcoin from "../assets/Icone/Bitcoin.svg";
 import Eth from "../assets/Icone/eth.svg";
@@ -90,7 +91,9 @@ export default function Home() {
             height={24}
             priority
           />
-          <Button className="bg-[#F7931A] rounded-sm">Explore</Button>
+          <Button className="bg-[#F7931A] rounded-sm">
+            <Link href="/blockchains">Explore</Link>
+          </Button>
         </div>
         <div className="flex flex-col items-center gap-[20px] w-[25%]">
           <Image
